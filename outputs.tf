8763f0e28f0ea65843a1d7e8a641a12edf90723f
Oliver Schoenborn <oliver@sentianSE.com>
2023-02-06 20:06:54 -0500

    add useful outputs

--- outputs.tf
+++ outputs.tf
@@ -1,20 +1,38 @@
-output "iam_policy_multi_stack_backends_common" {
-  value       = aws_iam_policy.multi_stack_backends_common.name
-  description = "Policy needed to access any state/bucket managed by this manager"
+output "manager_stack_id" {
+  value = local.manager_stack_id
 }
 
-output "iam_policy_multi_stack_backends_manager" {
-  value       = one(aws_iam_policy.multi_stack_backends_manager[*].name)
-  description = "Policy needed to access the state of this manager"
+output "dyndb_backend_locks_table" {
+  description = "Table that stores all the terraform tfstate backend locks for stacks managed by this module (including itself!)"
+  value = {
+    name = aws_dynamodb_table.backend_locks.name
+    arn  = aws_dynamodb_table.backend_locks.arn
+  }
 }
 
-output "iam_policy_multi_stack_backends_stacks" {
-  value       = [for p in aws_iam_policy.multi_stack_backends_stack : p.name]
-  description = "Policy needed to access stack-specific state managed by this manager"
+output "tfstate_backends_bucket" {
+  value = {
+    name          = aws_s3_bucket.tfstate_backends.id
+    region        = aws_s3_bucket.tfstate_backends.region
+    sse_kms_alias = aws_kms_alias.tfstate_backends.name
+  }
 }
 
-output "iam_policy_multi_stack_backends_modules" {
-  value       = [for p in aws_iam_policy.multi_stack_backends_module : p.name]
-  description = "Policy needed to access stack-module-specific state managed by this manager"
+output "replica_bucket" {
+  value = {
+    name          = aws_s3_bucket.replica.id
+    region        = aws_s3_bucket.replica.region
+    sse_kms_alias = aws_kms_alias.replica.name
+  }
 }
 
+output "access_control_iam_policies_for_tfstates" {
+  description = "Policies available to control access to any tfstate (in s3) managed by this manager"
+  value = {
+    common     = aws_iam_policy.multi_stack_backends_common.name
+    manager    = one(aws_iam_policy.multi_stack_backends_manager[*].name)
+    stacks     = [for p in aws_iam_policy.multi_stack_backends_stack : p.name]
+    sub_stacks = [for p in aws_iam_policy.multi_stack_backends_module : p.name]
+
+  }
+}
